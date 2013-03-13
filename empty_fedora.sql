c36b36bf7243d6094aebb47a7a7ce6cbd634de9e
Jonathan Green <jonathan@discoverygarden.ca>
2013-03-13 08:25:07 -0300

    Added xacml policies

---
+++ empty_fedora.sql
@@ -0,0 +1,9 @@
+use fedora;
+drop table datastreamPaths;
+drop table dcDates;
+drop table doFields;
+drop table doRegistry;
+drop table modelDeploymentMap;
+drop table objectPaths;
+drop table pidGen;
+
